232a68234ad539edf4dbb92e55749e7ee5f11660
Albert Dorador Chalar <trustalgorithm.dev@gmail.com>
2026-03-04 20:39:55 +0100

    Add files via upload

--- demo/app.py
+++ demo/app.py
@@ -58,17 +58,36 @@ st.dataframe(st.session_state.audit_sample, use_container_width=True)
 st.write("---")
 tab1, tab2 = st.tabs(["🔍 Single-Profile Explanation", "⚖️ Multi-Profile Comparison"])
 
+import matplotlib as mpl
+#import plotly.graph_objects as go
+#from plotly.io import renderers
+import os        
+from os.path import getmtime
 with tab1:
     row_id = st.selectbox("Select Row ID to Audit", st.session_state.audit_sample.index)
     if st.button("Generate Report", key="explain_btn"):
         x_df = st.session_state.audit_sample.loc[[row_id]].drop(columns=['charges'])
         x_orig = x_df.iloc[0]
         
-        # Capture the print output from the TRUSTRegressor class
-        explain_report = capture_output(model.explain, x_orig, mode="report")
+        # 1. Capture the print output from the TRUSTRegressor class
+        explain_report = capture_output(model.explain, x_orig, filename="Exp_demo")
         
+        # 2. Show it
         st.subheader("TRUST™ Prediction Audit Report")
         st.code(explain_report, language="text")
+
+        # 3. Grab and show plots
+        st.subheader("Supporting Charts")
+        plot_files = [f for f in os.listdir(".") if f.endswith("Exp_demo.png")]
+        if plot_files:
+            plot_files.sort(key=lambda x: getmtime(x))
+            cols = st.columns(2) # expects 2 plots will be generated
+            for idx, plot in enumerate(plot_files):
+                with cols[idx % 2]:
+                    st.image(plot, use_container_width=True)
+                    os.remove(plot)
+        else:
+            st.info("No charts available to display.")        
 
 with tab2:
     compare_ids = st.multiselect("Select 2 Row IDs to Compare", 
@@ -85,7 +104,6 @@ with tab2:
             x2 = st.session_state.audit_sample.loc[[compare_ids[1]]].drop(columns=['charges'])
             
             # 0. Prime backend
-            import matplotlib as mpl            
             mpl.use("module://matplotlib_inline.backend_inline")
             # 1. Capture the print output from the TRUSTRegressor class
             compare_report = capture_output(model.compare, x1, x2, filename="Comp_demo")
@@ -95,10 +113,6 @@ with tab2:
             st.code(compare_report, language="text")
             
             # 3. Grab and show plots
-            #import plotly.graph_objects as go
-            #from plotly.io import renderers
-            import os        
-            from os.path import getmtime
             st.subheader("Supporting Charts")
             plot_files = [f for f in os.listdir(".") if f.endswith("Comp_demo.png")]
             if plot_files:
